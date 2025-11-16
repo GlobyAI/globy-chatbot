491670d6715745523ff350971b391bf000e7fa8d
William Dinh <hoangdinh2411@gmail.com>
2025-11-16 13:44:41 +0100

    add a env varibale for base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({command})=>({
   plugins: [ reactRouter(), tsconfigPaths()],
   build:{
-    cssMinify:true
+    cssMinify:true,
+    ssr:true
   },
+  base:import.meta.env.VITE_BASE_PATH || '/',
   server: {
     port: 5173,
     host: '0.0.0.0',

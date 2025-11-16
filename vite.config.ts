8a89b9ea75302d1d1c3d60153d63b4f94ff6546c
William Dinh <hoangdinh2411@gmail.com>
2025-11-16 13:47:10 +0100

    fix issue reading env vars on config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { reactRouter } from '@react-router/dev/vite';
 
-export default defineConfig(({command})=>({
-  plugins: [ reactRouter(), tsconfigPaths()],
-  build:{
-    cssMinify:true,
-    ssr:true
-  },
-  base:import.meta.env.VITE_BASE_PATH || '/',
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-    allowedHosts: true
-  }
-}
-
-));
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [reactRouter(), tsconfigPaths()],
+    base: env.VITE_BASE_PATH || '/',
+    build: {
+      ssr: true,
+      outDir: 'build',
+      cssMinify: true,
+    },
+    server: {
+      port: 5173,
+      host: '0.0.0.0',
+      allowedHosts: true,
+    },
+  };
+});

bf1d980d95f629a6f940470b184b09af2215c5dc
William Dinh <hoangdinh2411@gmail.com>
2025-11-16 15:55:34 +0100

    change port to 3004

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
       cssMinify: true,
     },
     server: {
-      port: 5173,
+      port: 3004,
       host: '0.0.0.0',
       allowedHosts: true,
     },

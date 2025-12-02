2d68ba2ace12ca2d15dc6afc0cce1fcef24a81b5
William Dinh <hoangdinh2411@gmail.com>
2025-12-02 10:30:40 +0100

    fix issue  on deployment

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/chat/Chat.tsx"),
-  route("/price", "routes/pricing/pricing.tsx"),
+  index("routes/chat/chat.tsx"),
+  route("/price", "routes/pricing/Pricing.tsx"),
   route("/payment-process", "routes/payment-process/PaymentProcess.tsx"),
   route("/payment-success/:plan", "routes/payment-success/PaymentSuccess.tsx"),
   route("*", "routes/not-found/NotFound.tsx"),

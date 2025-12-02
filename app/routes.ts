a33abb3fc4f53af75ac14b888bf9eae3f361b629
William Dinh <hoangdinh2411@gmail.com>
2025-12-02 14:00:55 +0100

    change router path for paymnet success

--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,6 @@ export default [
   index("routes/chat/chat.tsx"),
   route("/price", "routes/pricing/Pricing.tsx"),
   route("/payment-process", "routes/payment-process/PaymentProcess.tsx"),
-  route("/payment-success/:plan", "routes/payment-success/PaymentSuccess.tsx"),
+  route("/success/:plan", "routes/payment-success/PaymentSuccess.tsx"),
   route("*", "routes/not-found/NotFound.tsx"),
 ] satisfies RouteConfig;
